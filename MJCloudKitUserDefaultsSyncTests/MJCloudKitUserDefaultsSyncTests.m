8a3ba707ce4652a24d7c313bf51e5a6e5c377302
Mark Jerde <mark.a.jerde@gmail.com>
2018-05-03 13:31:49 -0500

    Add unit test, which is piggybacked on normal usage since CloudKit requires entitlements.  It seems that xctest may not be able to test things that require entitlements.  Not ideal, but it gets the job done for now.

--- MJCloudKitUserDefaultsSyncTests/MJCloudKitUserDefaultsSyncTests.m
+++ MJCloudKitUserDefaultsSyncTests/MJCloudKitUserDefaultsSyncTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "MJCloudKitUserDefaultsSync.h"
 
 @interface MJCloudKitUserDefaultsSyncTests : XCTestCase
 
@@ -24,9 +25,8 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testSetAndRestoreValueThroughCloudKit {
+	XCTAssertEqual(0, 1, @"Please see UNIT_TEST_MEMORY_LEAKS in MJCloudKitUserDefaultsSync.m for unit test since this framework requires entitlements.");
 }
 
 - (void)testPerformanceExample {
